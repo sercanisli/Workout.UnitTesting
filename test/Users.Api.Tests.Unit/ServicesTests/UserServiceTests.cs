4ccd158ca813b136c29d52cae8457331c19229f5
Sercan ISLI <sercanislii@gmail.com>
2024-01-08 02:19:24 +0300

    Tested if GetAllAsync method returns empty list in UserManager.

--- test/Users.Api.Tests.Unit/ServicesTests/UserServiceTests.cs
+++ test/Users.Api.Tests.Unit/ServicesTests/UserServiceTests.cs
@@ -1,6 +1,36 @@
-﻿namespace Users.Api.Tests.Unit.ServicesTests
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Users.Api.Models;
+using Users.Api.Repositories;
+using Users.Api.Services;
+
+namespace Users.Api.Tests.Unit.ServicesTests
 {
     public class UserServiceTests
     {
+        private readonly UserManager _sut;
+        private readonly IUserRepository _userRepository = Substitute.For<IUserRepository>();
+        private readonly ILogger<User> _logger = Substitute.For<ILogger<User>>();
+        private readonly IMapper _mapper;
+
+        public UserServiceTests()
+        {
+            _sut = new(_userRepository, _logger, _mapper);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldReturnEmptyList_WhenNoUserExist()
+        {
+            //Arrange
+            _userRepository.GetAllAsync().Returns(Enumerable.Empty<User>().ToList());
+
+            //Act
+            var result = await _sut.GetAllAsync();
+
+            //Assert
+            result.Should().BeEmpty();
+        }
     }
 }
