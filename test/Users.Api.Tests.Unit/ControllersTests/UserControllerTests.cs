261f145b863c7b6f951d5f6b02b40e6dacd6d247
Sercan ISLI <sercanislii@gmail.com>
2024-01-11 01:12:35 +0300

    Tested GetAll method in UsersController.

--- test/Users.Api.Tests.Unit/ControllersTests/UserControllerTests.cs
+++ test/Users.Api.Tests.Unit/ControllersTests/UserControllerTests.cs
@@ -1,6 +1,33 @@
-﻿namespace Users.Api.Tests.Unit.ControllersTests
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Users.Api.Controllers;
+using Users.Api.Models;
+using Users.Api.Services;
+
+namespace Users.Api.Tests.Unit.ControllersTests
 {
     public class UserControllerTests
     {
+        private readonly UsersController _sut;
+        private readonly IUserService _userService = Substitute.For<IUserService>();
+
+        public UserControllerTests()
+        {
+            _sut = new(_userService);
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnUsers()
+        {
+            //Arrange
+            _userService.GetAllAsync().Returns(Enumerable.Empty<User>().ToList());
+
+            //Act
+            var result = (OkObjectResult)await _sut.GetAll(default);
+
+            //Assert
+            result.StatusCode.Should().Be(200);
+        }
     }
 }
